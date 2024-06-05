8ecb5481e441ebeb5da331d94eae46be85d45426
Dan <profedanieljuan@gmail.com>
2024-06-05 11:21:33 -0300

    Se termino de actualizar el htm y js segun el video mandado por el profe para trabajar con api

--- api.js
+++ api.js
@@ -8,7 +8,20 @@ fetch("https://rickandmortyapi.com/api/character")
     console.log(datos.results)
 
     datos.results.forEach(elementos => {
-        console.log(elementos.name)
+       console.log(elementos.name)
+       console.log(elementos.image)
+
+    const contenedorCreado = document.createElement('div')
+        //Usa bastic por eso las comillas invertidas, y ademas el uso del signo $
+    contenedorCreado.innerHTML = `
+        <h4>${elementos.name}</h4>
+        <img src="${elementos.image}">
+    
+    
+    
+    `;
+
+    contenedorPersonajes.append(contenedorCreado)
         
     });
 
