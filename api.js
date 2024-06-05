1afddf73dede00c622d756477bda5f8f8fab3aec
Dan <profedanieljuan@gmail.com>
2024-06-05 10:37:52 -0300

    Actualizacion de api.html y api.js

--- api.js
+++ api.js
@@ -1,1 +1,15 @@
-a
+let contenedorPersonajes = document.getElementById("Personajes")
+
+fetch("https://rickandmortyapi.com/api/character")
+.then((response)=>response.json())
+.then((datos)=>{
+
+    console.log(datos)
+    console.log(datos.results)
+
+    datos.results.forEach(elementos => {
+        console.log(elementos.name)
+        
+    });
+
+})
